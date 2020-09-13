cb62c893b36eec02944136ca67433add5c3b11aa
Robert Bolling <robboles1014@gmail.com>
2020-09-13 19:53:03 -0400

    Completed fibonacci testing

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -34,7 +34,6 @@ class TestKatas(unittest.TestCase):
         self.assertEqual(multiply(num, num2), num * num2)
         self.assertEqual(multiply(neg_num, num2), neg_num * num2)
         self.assertEqual(multiply(neg_num, neg_num2), neg_num * neg_num2)
-        # self.fail("TODO: Write multiply unit test")
 
     def test_power(self):
         '''Return the input exponent of an input number'''
@@ -44,16 +43,20 @@ class TestKatas(unittest.TestCase):
         self.assertRaises(ValueError, power, 5, fraction())
         self.assertEqual(power(9, 3), 729)
         self.assertEqual(power(num, num2), num ** num2)
-        # self.fail("TODO: Write power unit test")
 
     def test_factorial(self):
+        num = rand_positive_int(1, 1000)
         self.assertRaises(ValueError, factorial, rand_negative_int())
         self.assertEqual(factorial(5), 120)
+        self.assertEqual(factorial(num), math.factorial(int(num)))
         for n in range(0, 15):
             self.assertEqual(factorial(n), math.factorial(int(n)))
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        fib_30 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229]
+        self.assertRaises(ValueError, fibonacci, rand_negative_int())
+        for n, fib in enumerate(fib_30):
+            self.assertEqual(fibonacci(n), fib)
 
 
 if __name__ == '__main__':
